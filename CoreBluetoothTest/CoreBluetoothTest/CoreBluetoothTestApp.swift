79850f029a0db90faddc35f6f12e0882a8cde98c
sun <sts07190@naver.com>
2026-05-27 11:42:44 +0900

    [chore/#1] Bluetooth 권한 및 Background Mode 설정 추가

---
+++ CoreBluetoothTest/CoreBluetoothTest/CoreBluetoothTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothTestApp.swift
+//  CoreBluetoothTest
+//
+//  Created by sun on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreBluetoothTest/CoreBluetoothTestWatch Watch App/CoreBluetoothTestWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothTestWatchApp.swift
+//  CoreBluetoothTestWatch Watch App
+//
+//  Created by sun on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothTestWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
